00e40a3a341932a3e88f3bdff3c9db3672bcaa8c
GentianaRamadani <GentianaRamadani@GENTA>
2026-04-01 17:09:28 +0200

    Task 6

--- utils.py
+++ utils.py
@@ -1,3 +1,5 @@
+CELL_SIZE = 40
+
 def draw_maze(canvas, maze, visited=set(), path=[]):
     canvas.delete("all")
 
@@ -9,3 +11,21 @@ def draw_maze(canvas, maze, visited=set(), path=[]):
             y2 = y1 + CELL_SIZE
 
             cell = maze[i][j]
+
+            if cell == 1:
+                color = "#1e1e1e"  # mur
+            elif (i, j) in path:
+                color = "#ffd166"  # rruga finale
+            elif (i, j) in visited:
+                color = "#4cc9f0"  # e vizituar
+            else:
+                color = "#f8f9fa"  # bosh
+
+            if cell == 'S':
+                color = "#06d6a0"
+            elif cell == 'G':
+                color = "#ef476f"
+
+            canvas.create_rectangle(x1, y1, x2, y2, fill=color, outline="#adb5bd")
+
+    canvas.update()   
